050f356f03869984c502f1bd026e5f29b8279b8c
Bryan <morganthemosaic@gmail.com>
2024-08-27 23:04:37 -0400

    Add initial layout with two styled views

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+	return (
+		<View style={styles.container}>
+			<View style={styles.new}></View>
+			<View style={styles.old}></View>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: { flex: 1, width: "100%", flexDirection: "column" },
+	new: {
+		backgroundColor: "#90E0EF",
+		flex: 1,
+		width: "100%",
+		height: "100%"
+	},
+	old: { backgroundColor: "#0077B6", flex: 1, width: "100%", height: "100%" }
 });
+
+export default App;
