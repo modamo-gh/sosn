4ff8fe86b6a5f98d7300eb9608a8637ff1a80997
Bryan <morganthemosaic@gmail.com>
2024-08-28 07:33:44 -0400

    Change size of section based on press

--- App.tsx
+++ App.tsx
@@ -1,23 +1,33 @@
-import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { Pressable, StyleSheet, View } from "react-native";
 
 const App = () => {
+	const [focusedSection, setFocusedSection] = useState<string>("");
+
 	return (
 		<View style={styles.container}>
-			<View style={styles.new}></View>
-			<View style={styles.old}></View>
+			<Pressable
+				onPress={() => setFocusedSection("new")}
+				style={[styles.new, focusedSection === "new" && styles.focused]}
+			></Pressable>
+			<Pressable
+				onPress={() => setFocusedSection("old")}
+				style={[styles.old, focusedSection === "old" && styles.focused]}
+			></Pressable>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	container: { flex: 1, width: "100%", flexDirection: "column" },
+	container: { flex: 1, flexDirection: "column", width: "100%" },
+	focused: {
+		flex: 4
+	},
 	new: {
 		backgroundColor: "#90E0EF",
-		flex: 1,
-		width: "100%",
-		height: "100%"
+		flex: 1
 	},
-	old: { backgroundColor: "#0077B6", flex: 1, width: "100%", height: "100%" }
+	old: { backgroundColor: "#0077B6", flex: 1 }
 });
 
 export default App;
